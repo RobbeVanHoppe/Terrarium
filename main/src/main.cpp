e183ac19f2ee37f86b010745346dee723ac30d2d
robbevanhoppe <robbevanhoppe@outlook.com>
2024-11-29 12:55:57 +0100

    setup and read temp with ds18b20 sensor

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -19,21 +19,50 @@
 // }
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "ds18x20.h"
+#include "esp_log.h"
 
-extern "C" void app_main(void) {
-        ds18x20_addr_t sensor_addr = DS18X20_ANY;
-        float temperature;
-        esp_err_t res = ds18x20_measure_and_read(GPIO_NUM_4, sensor_addr, &temperature);
+const gpio_num_t sensor_gpio = GPIO_NUM_4;
+const size_t addr_count = 1;
+size_t found_addr_count;
+onewire_addr_t addr_list[addr_count];
 
-        while (1) {
-            if (res == ESP_OK) {
-                printf("Temperature: %.2f\n", temperature);
-            } else {
-                printf("Could not read temperature\n");
-            }
-        vTaskDelay(pdMS_TO_TICKS(1000));
+float temperature;
 
-        res = ds18x20_measure_and_read(GPIO_NUM_4, sensor_addr, &temperature);
+esp_err_t setup_temperature()
+{
+    // Initialize the GPIO
+    if (ds18x20_scan_devices(sensor_gpio, addr_list, addr_count, &found_addr_count) != ESP_OK)
+    {
+        ESP_LOGE("DS18B20", "No sensors detected!");
+        return ESP_FAIL;
+    }
+    // Print the address of the sensor found on the bus
+    ESP_LOGI("DS18B20", "Sensor found at address: 0x%016llx", addr_list[0]);
+    return ESP_OK;
+}
+
+void read_temperature()
+{
+    if (ds18x20_measure_and_read(sensor_gpio, addr_list[0], &temperature) != ESP_OK)
+    {
+        ESP_LOGE("DS18B20", "Could not read temperature!");
+        return;
+    }
+    else
+    {
+        ESP_LOGI("DS18B20", "Temperature: %.2f", temperature);
+    }
+}
+
+extern "C" void app_main(void)
+{
+    // scan and setup ds18b20 temp sensors
+    setup_temperature();
+
+    while (1)
+    {
+        read_temperature();
     }
 }
